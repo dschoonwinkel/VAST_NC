facdb60502e8d6d66c0a5bfaec5ab158d07523c4
Daniel Xubuntu <daniel.schoonwinkel@gmail.com>
2019-01-28 17:45:16 +0200

    Getting scripts ready to process commulative data.

--- bin/scripts/plot_results.py
+++ bin/scripts/plot_results.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plot
 import numpy as np
 import csv
+import sys
 
 TIMESTAMP = 0
 ACTIVE_NODES = 1
@@ -15,7 +16,6 @@ WORLDRECVSTAT = 8
 x_axis_interval = 20000
 
 results_text = list()
-
 
 with open('../logs/results/results1.txt', 'r') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=",")
@@ -39,6 +39,7 @@ numpy_results = np.array(results)
 # print("Timestamps: ", numpy_results[:,0])
 
 timestamps = numpy_results[:,0]
+first_timestamp = int(results_text[0][0])
 
 plot.figure(1, figsize=(12, 10), dpi=80)
 
@@ -53,8 +54,10 @@ plot.xlim(0, max(timestamps)+1)
 plot.grid(True)
 
 plot.subplot(4,1,2)
-topo_consistency = 100* 1.0*numpy_results[:,3] / (1.0*numpy_results[:,3])
+topo_consistency = 100* 1.0*numpy_results[:,3] / (1.0*numpy_results[:,2])
 plot.plot(timestamps, topo_consistency)
+where_is_finite = np.isfinite(topo_consistency)
+mean_consistency = np.mean(topo_consistency[where_is_finite])
 where_are_NaNs = np.isnan(topo_consistency)
 topo_consistency[where_are_NaNs] = 100
 plot.plot(timestamps[where_are_NaNs], topo_consistency[where_are_NaNs], 'r,')
@@ -67,6 +70,9 @@ plot.subplot(4,1,3)
 drift_nodes = numpy_results[:,DRIFT_NODES]
 drift_distance = numpy_results[:,TOTAL_DRIFT]
 normalised_drift_distance = drift_distance / drift_nodes
+
+mean_drift_distance = np.mean(normalised_drift_distance[where_is_finite])
+
 plot.plot(timestamps, normalised_drift_distance)
 plot.ylabel("Norm drift distance")
 plot.xticks(np.arange(min(timestamps), max(timestamps)+1, x_axis_interval))
@@ -79,7 +85,7 @@ recv_stat = numpy_results[:,8]
 plot.plot(timestamps, send_stat, 'g',label='Send stat')
 # plot.plot(timestamps, recv_stat, 'b', label='Recv stat')
 plot.ylabel("Send/recv stats [B]")
-plot.xlabel("Timestamp")
+plot.xlabel("Timestamp [ms]")
 # plot.xticks(np.arange(min(timestamps), max(timestamps)+1, x_axis_interval))
 plot.xticks(np.arange(min(timestamps), max(timestamps)+1, x_axis_interval))
 plot.legend()
@@ -88,6 +94,12 @@ plot.xlim(0, max(timestamps)+1)
 
 plot.savefig("../logs/results/active_nodes_topo_cons_.pdf", dpi=300)
 plot.savefig("../logs/results/active_nodes_topo_cons_.png", dpi=300)
+
+if (len(sys.argv) > 1):
+	print("Saving test results in results_summary.txt with label " + sys.argv[1])
+
+	with open('../logs/results/results_summary.txt', 'a') as outfile:
+		outfile.write(("%s, " + sys.argv[1] + ", %f, %f, %f, %f, %f\n") % (first_timestamp, np.max(active_nodes), mean_consistency, mean_drift_distance, np.mean(send_stat), np.mean(recv_stat)))
 
 plot.show()
 
