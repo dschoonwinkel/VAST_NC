a14cf8333974a9d5d556332b02bfaa956f594393
Daniel Xubuntu <daniel.schoonwinkel@gmail.com>
2019-03-18 09:56:58 +0200

    Added RLNCMessge functionality. Will see if it breaks soon

--- VASTnet/rlncmessage.cpp
+++ VASTnet/rlncmessage.cpp
@@ -13,15 +13,40 @@ RLNCMessage::RLNCMessage (RLNCHeader header)
     header.enc_packet_count = 0;
 }
 
-std::vector<packetid_t> RLNCMessage::getPacketIds()
+RLNCMessage::RLNCMessage (const RLNCMessage &message)
+{
+    this->header = message.header;
+    this->msg = new char[message.header.packetsize];
+    memcpy(this->msg, message.msg, message.header.packetsize);
+    this->pkt_ids = message.pkt_ids;
+    this->from_ids = message.from_ids;
+}
+
+const std::vector<packetid_t> RLNCMessage::getPacketIds()
 {
     return pkt_ids;
 }
 
-void RLNCMessage::putPacketId(packetid_t pkt_id)
+const std::vector<Vast::id_t> RLNCMessage::getFromIds()
+{
+    return from_ids;
+}
+
+Vast::id_t RLNCMessage::getFirstFromId()
+{
+    return from_ids.front ();
+}
+
+void RLNCMessage::putPacketId(packetid_t pkt_id, bool autonumber)
 {
     pkt_ids.push_back(pkt_id);
-    header.enc_packet_count = pkt_ids.size();
+    if (autonumber)
+        header.enc_packet_count = pkt_ids.size();
+}
+
+void RLNCMessage::putFromId (Vast::id_t from_id)
+{
+    from_ids.push_back(from_id);
 }
 
 void RLNCMessage::putMessage(const char* buffer, size_t len)
@@ -44,11 +69,32 @@ char* RLNCMessage::getMessage(size_t len)
     return msg;
 }
 
+uint8_t* RLNCMessage::getMessageU (size_t len)
+{
+    return reinterpret_cast<uint8_t*>(getMessage(len));
+}
+
+size_t RLNCMessage::getMessageSize()
+{
+    return header.packetsize;
+}
+
+void RLNCMessage::putOrdering(uint8_t ordering)
+{
+    header.ordering = ordering;
+}
+
+uint8_t RLNCMessage::getOrdering()
+{
+    return header.ordering;
+}
+
 // size of this class, must be implemented
 size_t RLNCMessage::sizeOf ()
 {
     size_t size = sizeof(RLNCHeader);
     size += header.enc_packet_count * sizeof(packetid_t);
+    size += header.enc_packet_count * sizeof(Vast::id_t);
     size += header.packetsize;
 
     return size;
@@ -70,6 +116,8 @@ size_t RLNCMessage::serialize (char *buffer)
     {
         memcpy(buffer+n, &pkt_ids[i], sizeof(packetid_t));
         n += sizeof(packetid_t);
+        memcpy(buffer+n, &from_ids[i], sizeof(Vast::id_t));
+        n += sizeof(Vast::id_t);
     }
     memcpy(buffer+n, msg, header.packetsize);
     n += header.packetsize;
@@ -80,7 +128,7 @@ size_t RLNCMessage::serialize (char *buffer)
 
 // restores a buffer into the object
 // returns number of bytes restored (should be > 0 if correct)
-size_t RLNCMessage::deserialize (const char *buffer, size_t size)
+int RLNCMessage::deserialize (const char *buffer, size_t size)
 {
     size_t n = 0;
     if (size > sizeof(RLNCHeader) && buffer != NULL)
@@ -88,13 +136,43 @@ size_t RLNCMessage::deserialize (const char *buffer, size_t size)
         memcpy(&header, buffer+n, sizeof(RLNCHeader));
         n += sizeof(RLNCHeader);
 
+        if (!RLNCHeader_factory::isRLNCHeader (header))
+        {
+            CPPDEBUG("RLNCMessage::deserialize Not a RLNC Header" << std::endl);
+
+            std::cout << "Sample of first few bytes in buffer (max =" << size << std::endl;
+            for (size_t i = 0; i < 10; i++)
+                printf("%x.", buffer[i] & 0xff);
+
+            std::cout << std::endl;
+
+
+            return -1;
+        }
+
         packetid_t pkt_id = -1;
+        Vast::id_t from_id = -1;
         for (int i = 0; i < header.enc_packet_count; i++)
         {
             memcpy(&pkt_id, buffer+n, sizeof(packetid_t));
-            putPacketId(pkt_id);
+            putPacketId(pkt_id, false);
             n += sizeof(packetid_t);
+            memcpy(&from_id, buffer+n, sizeof(Vast::id_t));
+            putFromId (from_id);
+            n += sizeof(Vast::id_t);
         }
+        if (header.packetsize > size)
+        {
+            std::cerr << "RLNCMessage::deserialize Header packet size larger than available bytes" << std::endl;
+            std::cerr << "header.start " << header.start << std::endl;
+            std::cerr << "header.end: " << header.end << std::endl;
+            msg = new char[size - n];
+            memcpy(msg, buffer+n, size-n);
+            std::cerr << "RLNCMessage::deserialize" << *(this) << std::endl;
+            return -1;
+        }
+
+
         msg = new char[header.packetsize];
         memcpy(msg, buffer+n, header.packetsize);
 
@@ -107,10 +185,62 @@ size_t RLNCMessage::deserialize (const char *buffer, size_t size)
 
     return 0;
 }
+
+bool RLNCMessage::operator==(const RLNCMessage other)
+{
+    bool equals = true;
+
+    equals = equals && (RLNCHeader_factory::isRLNCHeadersEqual (other.header, this->header));
+    equals = equals && (other.pkt_ids == this->pkt_ids);
+    equals = equals && (other.from_ids == this->from_ids);
+
+    if (other.header.packetsize != this->header.packetsize)
+        return false;
+
+    for (size_t i = 0; i < this->header.packetsize; i++)
+    {
+        equals = equals && (other.msg[i] == this->msg[i]);
+    }
+
+    return equals;
+}
+
 std::ostream& operator<<(std::ostream& output, RLNCMessage const& message )
 {
+
         output << "RLNCMessage::stream >> output: ******************************\n";
+        output << "Header: " << std::endl;
+        output << "header.ordering " << message.header.ordering<< std::endl;
+        output << "header.packetsize " << message.header.packetsize << std::endl;
+        output << "header.gensize " << message.header.gensize << std::endl;
+        output << "header.generation " << message.header.generation << std::endl;
+        output << "header.enc_pack_count " << message.header.enc_packet_count << std::endl;
+        for (size_t i = 0; i < message.pkt_ids.size(); i++)
+        {
+            output << "Pkt_ids[" << i << "]" << message.pkt_ids[i] << std::endl;
+            output << "From id[" << i << "]" << message.from_ids[i] << std::endl;
+        }
+        output << "msg: " << std::endl;
+        boost::format format("%1$#x");
+        for (int i = 0; i < std::min<unsigned short>(message.header.packetsize, 10); i++)
+        {
+            output << format % message.msg[i];
+        }
+        output << std::endl;
 
         output << "******************************************************" << std::endl;
         return output;
+}
+
+packetid_t RLNCMessage::generatePacketId(Vast::id_t id, int ordering)
+{
+    packetid_t x = id;
+    x += 0x9e3779b97f4a7c15;
+    x ^= (x >> 30);
+    x *= 0xbf58476d1ce4e5b9;
+    x ^= (x >> 27);
+    x *= 0x94d049bb133111eb;
+    x ^= (x >> 31);
+    x += ordering;
+    return x;
 }
--- VASTnet/rlncmessage.h
+++ VASTnet/rlncmessage.h
@@ -15,23 +15,40 @@ public:
     RLNCMessage();
 
     RLNCMessage (RLNCHeader header);
+    //Copy constructor
+    RLNCMessage (const RLNCMessage &message);
 
-    std::vector<packetid_t> getPacketIds();
+    const std::vector<packetid_t> getPacketIds();
+    const std::vector<Vast::id_t> getFromIds();
+    Vast::id_t getFirstFromId();
 
-    void putPacketId(packetid_t pkt_id);
+    void putPacketId(packetid_t pkt_id, bool autonumber = true);
+    void putFromId(Vast::id_t from_id);
 
     void putMessage(const char* buffer, size_t len);
 
     char* getMessage(size_t len = 0);
+    uint8_t* getMessageU(size_t len = 0);
+    size_t getMessageSize();
+
+    void putOrdering(uint8_t ordering);
+    uint8_t getOrdering();
 
     size_t sizeOf ();
     size_t serialize (char *buffer);
-    size_t deserialize (const char *buffer, size_t size);
+    //Returns -1 on error
+    int deserialize(const char *buffer, size_t size);
+
+    bool operator==(const RLNCMessage other);
+
+    packetid_t static generatePacketId(Vast::id_t id, int ordering);
+
     friend std::ostream& operator<<(std::ostream&, RLNCMessage const& log);
 
 private:
     RLNCHeader header;
     std::vector<packetid_t> pkt_ids;
+    std::vector<Vast::id_t> from_ids;
     char* msg;
 
 };
