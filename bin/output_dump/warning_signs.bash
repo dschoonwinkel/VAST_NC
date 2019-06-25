f0d8c1bd6cf100f08a3d9594d7206bd1babe403c
Daniel Schoonwinkel <daniel.schoonwinkel@gmail.com>
2019-06-25 10:15:31 +0200

    warning_signs.bash shows possible errors in log ouput and mount_VASTHorus.bash mounts sshfs of horus VAST output directory.

---
+++ bin/output_dump/warning_signs.bash
@@ -0,0 +1,19 @@
+#/bin/bash
+
+grep -r "AddressSanitizer" *.txt | uniq -c
+
+grep -r "Subsc" *.txt | uniq -c
+grep -r "Subsc" *.txt | sort | uniq | wc
+
+grep -r "isJoined == false" *.txt
+grep -r "remove" *.txt | sort | uniq
+# grep -r "VASTMatcher::removeFailedSubscribers" *.txt
+# grep -r "VASTClient::removeGhosts" *.txt
+grep -r "No connection to" *.txt
+
+echo "Number of forwarded RELAY_QUERY's"
+grep -r "meant for" *.txt | wc 
+
+grep -r "discarding packet to " *.txt
+# grep -r "Empty RLNC" *.txt | sort | uniq -c
+grep -r "removeExpiredMatchers" *.txt
