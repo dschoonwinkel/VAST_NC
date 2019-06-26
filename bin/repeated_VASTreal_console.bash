c6a89ec1bdb67dd2ebc80f87c9f5f556fe20e65a
Daniel Horus <daniel.schoonwinkel@gmail.com>
2019-06-26 16:33:02 +0200

    Changed repeated to use correct Linux nc -N commands.

--- bin/repeated_VASTreal_console.bash
+++ bin/repeated_VASTreal_console.bash
@@ -2,13 +2,13 @@
 for i in {1..5}
 do
 	echo "Run $i"
-	# make
+	make
 	label=$(./generate_label.py)
 	echo $label
 	sudo ../mininet_topos/Multinode_pox_codinghost.py
 	./vaststat_replay
 	make backup_results foo=$label
-	echo "Run $i completed $(HOSTNAME) $label"
-	echo "Run $i completed $(HOSTNAME) $label" | nc -c horus.local 12345 
+	echo "Run $i completed $HOSTNAME $label"
+	echo "Run $i completed $HOSTNAME $label" | nc -N horus.local 12345 
 
 done
