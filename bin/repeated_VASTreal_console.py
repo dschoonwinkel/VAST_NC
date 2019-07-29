0f036dc3f7586b03413fdda85cea0402f1bebb22
Daniel Schoonwinkel <daniel.schoonwinkel@gmail.com>
2019-07-29 11:38:25 +0200

    Added emailing to repeated_VASTreal_console.py script

--- bin/repeated_VASTreal_console.py
+++ bin/repeated_VASTreal_console.py
@@ -40,12 +40,17 @@ def replace_NODECOUNT(node_count):
 NODE_COUNT_list = [5, 10, 11, 15]
 iterations = 5
 
-#Run over all NODE_COUNT options
-# for i in range(len(NODE_COUNT_list)):
-# 	replace_NODECOUNT(NODE_COUNT_list[i])
-# 	#Run iterations
-# 	for j in range(iterations):
-# 		label = runOnce()
-# 		print("Run %d completed %s %s" % (i, socket.gethostname(), label))
+# Run over all NODE_COUNT options
+for i in range(len(NODE_COUNT_list)):
+	replace_NODECOUNT(NODE_COUNT_list[i])
+	#Run iterations
+	for j in range(iterations):
+		label = runOnce()
+		print("Run %d completed %s %s" % (i, socket.gethostname(), label))
+
+	subprocess.call("echo \"Runs completed on %s with label %s\" \
+		| mail -s \"Test status\" daniel.schoonwinkel@gmail.com" % 
+		(socket.gethostname(), label),
+			shell=True)
 
 print(runOnce())
