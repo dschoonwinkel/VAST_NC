7ce2b24755b8675751bae427a8a97ffe64666d63
Daniel LabPC Linux <daniel.schoonwinkel@gmail.com>
2019-08-02 14:21:16 +0200

    Added repeated_NETMODELs_Docker

--- bin/scripts/parse_all_backupresults.bash
+++ bin/scripts/parse_all_backupresults.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for file in $(find $HOME/Development/VAST-0.4.6/bin/logs/logs_horus | grep results1.txt)
+for file in $(find $HOME/Development/VAST-0.4.6/bin/logs/logs_backup | grep results1.txt)
 do
 echo $file
 plot_results.py $file
