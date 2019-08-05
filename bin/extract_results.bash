b156165d600fec2927eb04b4c0c7ff60fb8fd14f
Daniel Schoonwinkel <daniel.schoonwinkel@gmail.com>
2019-08-05 16:05:03 +0200

    Added results1_resources.txt to extract_results.bash:

--- bin/extract_results.bash
+++ bin/extract_results.bash
@@ -19,3 +19,21 @@ mkdir ../../results_extracted &> /dev/null
 cp $NAME ../../results_extracted/$NAME4
 
 done
+
+for output in $(find . | grep results1_resources.txt)
+do 
+
+# echo $output 
+
+NAME=$output
+echo $NAME
+NAME2=${NAME:2}
+NAME3=${NAME2/"results/"/}
+NAME4=${NAME3/'/'/_}
+
+echo $NAME4
+
+mkdir ../../results_extracted &> /dev/null
+cp $NAME ../../results_extracted/$NAME4
+
+done
