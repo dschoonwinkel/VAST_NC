6e1a166687198bba9250f26ca715fa22b0a7a6e1
= <=>
2019-08-06 10:02:27 +0200

    Changed rerun_vaststatreplay to handle directories correctly

--- bin/logs/logs_backup/rerun_vaststatreplay.bash
+++ bin/logs/logs_backup/rerun_vaststatreplay.bash
@@ -1,13 +1,20 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 for folder in $(ls)
 do
 echo $folder
-cd $folder
+if [ -d $folder ];
+then
+else
+    echo "$folder is not a directory"
+    continue
+fi
+
+cd ./$folder
 mkdir logs
 mkdir logs/results
 mv *.txt logs
-cp ../../../VASTreal.ini ./
-cp ../../../Mininet.ini ./
+cp -n ../../../VASTreal.ini ./
+cp -n ../../../Mininet.ini ./
 vaststat_replay
 cp logs/results/results1.txt ./results/
 rm logs/results/results1.txt
