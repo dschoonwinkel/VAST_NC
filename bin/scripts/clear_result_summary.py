67a70aa1a68a6978e8283622ae37682220d6b6b8
Daniel Schoonwinkel <daniel.schoonwinkel@gmail.com>
2019-08-12 14:43:52 +0200

    Added MCrecv to header in clear_result_summary.py

--- bin/scripts/clear_result_summary.py
+++ bin/scripts/clear_result_summary.py
@@ -7,4 +7,4 @@ print("Home Dir: ", home_dir)
 
 # Reset results_summary.py
 with open('%s/Development/VAST-0.4.6/bin/results_summary/results_summary.txt' % home_dir, 'w') as csvfile:
-    csvfile.write('timestamp, NET_MODEL, NODECOUNT, BW, DELAY, LOSS, STEPS, PLATFORM, ACTIVE_NODES, TOPO_CONS, DRIFTDISTANCE, SEND_BW, RECV_BW\n')
+    csvfile.write('timestamp, NET_MODEL, NODECOUNT, BW, DELAY, LOSS, STEPS, PLATFORM, ACTIVE_NODES, TOPO_CONS, DRIFTDISTANCE, SEND_BW, RECV_BW, RAWMC_RECV, USEDMC_RECV\n')
