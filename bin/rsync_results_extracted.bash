89886760228f96811f17429bdf7c2d3ca6d63233
Daniel Horus <daniel.schoonwinkel@gmail.com>
2019-08-22 12:51:20 +0200

    Changed order of repeated run to be 50, 40, 30, 20, 10 nodes and NET_UDPNC, NET_UDP, NET_ACE. Most important results complete first..

--- bin/rsync_results_extracted.bash
+++ bin/rsync_results_extracted.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "Rsyncing results_summary.txt"
 rsync -rv ~/Development/VAST-0.4.6/bin/results_summary/results_summary.txt schoonwi@10.10.11.81:~/Development/VAST-0.4.6/bin/logs/logs_$(hostname)/
+./extract_results.py
 echo "Rsyncing results_extracted"
 rsync --progress -vr ~/Development/VAST-0.4.6/bin/results_extracted/* schoonwi@10.10.11.81:~/Development/VAST-0.4.6/bin/logs/logs_$(hostname)/results_extracted/
