c4dde7639f3d7ea51f548b9fa954fc48482b5b8d
Daniel Horus <daniel.schoonwinkel@gmail.com>
2019-09-30 11:37:01 +0200

    Added more options to repeated_NETMODELs.py and repeated_VASTreal_console.py

--- bin/repeated_VASTreal_console.py
+++ bin/repeated_VASTreal_console.py
@@ -40,6 +40,7 @@ def replace_NODECOUNT(node_count):
 
 def main():
 	#NODE_COUNT_list = [50, 40, 30, 20, 10]
+	#NODE_COUNT_list = [40, 30, 20, 10]
 	NODE_COUNT_list = [50]
 	# NODE_COUNT_list = [15]
 
